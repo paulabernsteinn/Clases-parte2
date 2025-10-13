3e83eace7924161bec61eaeac402f524db6228d0
paulabernsteinn <bernsteinp@udesa.edu.ar>
2025-10-13 18:34:47 -0300

    intento de la ultima parte

--- Clase.09/practica3ej3/src/Components/Card.js
+++ Clase.09/practica3ej3/src/Components/Card.js
@@ -5,10 +5,9 @@ import { View } from 'react-native-web';
 function Card(props){
     return(
         <View style={styles.principal}>
-            <Text >{props.info.name}</Text>
-            <Text>{props.info.price}</Text>
-            <Text>{props.info.description}</Text>
-            <Text>{props.info.category}</Text>
+            <Text >{props.info.title}</Text>
+            <Text>{props.info.species}</Text>
+            <Text>{props.info.gender}</Text>
             <Image style={styles.image} 
                     source={{uri:props.info.image}}
                     resizeMode='contain'/>
---
+++ Clase.09/practica3ej3/src/Screens/ProductsAll.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import {View, Text, FlatList} from 'react-native';
+import { StyleSheet } from 'react-native';
+import Card from '../Components/Card';
+
+
+class ProductsAll extends Component{
+   constructor(props){
+        super(props)
+         this.state={
+          data: []
+        }}
+componentDidMount(){
+      fetch('https://rickandmortyapi.com/api/character')
+      .then((res) => res.json())
+      .then((data) => {
+          this.setState({
+            data: data.results,
+            loading:false,
+   });
+ })
+ .catch((error) => console.log(error));
+
+}
+
+    render(){
+        return(
+            <View style={styles.flatlist}>
+                <Text>Lista de productos</Text>
+                <FlatList 
+                    data={ this.state.array }
+                    keyExtractor={ item => item.id.toString() }
+                    renderItem={ ({item}) => <Card info={item}/> }
+                />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+   flatlist: {
+    	width: '100%',
+flex: 1
+   },
+})
+
+
+export default ProductsAll
